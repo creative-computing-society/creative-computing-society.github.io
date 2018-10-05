8387a00388de4afdff5dbc1d331fbd7083f76cc7
sourav kumar <souravs_1999@rediffmail.com>
2018-10-05 19:40:17 +0530

    added the scrolltotop button
    have added a scroll to top button .

--- js/creative.js
+++ js/creative.js
@@ -90,3 +90,38 @@
   });
 
 })(jQuery); // End of use strict
+
+.scrollUpButton {
+ display: none;
+ opacity: 0.6;
+ position: fixed;
+ bottom: 10px;
+ right: 10px;
+ display: none;
+ background: #000;
+ color: #fff;
+ font-size: 1.5em;
+ text-decoration: none;
+ padding: 5px 10px 5px 10px;
+}
+.scrollUpButton:hover, .scrollUpButton:focus {
+ outline: none;
+ text-decoration: none;
+ color: #fff;
+opacity: 1;}
+
+// scrollUpButton
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if ($(this).scrollTop() > 100) {
+          $('.scrollUpButton').fadeIn();
+      } else {
+          $('.scrollUpButton').fadeOut();
+      }
+  });
+  $('.scrollUpButton').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+  });
+ });
