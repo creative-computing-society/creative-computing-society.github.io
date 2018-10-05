e9b9d52b36fcb8b73746e4b2de1794d697407e21
sourav kumar <souravs_1999@rediffmail.com>
2018-10-05 23:24:10 +0530

    reverting to original

--- js/creative.js
+++ js/creative.js
@@ -93,18 +93,3 @@
 
 
 
-// scrollUpButton
-
-$(document).ready(function(){
-  $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-          $('.scrollUpButton').fadeIn();
-      } else {
-          $('.scrollUpButton').fadeOut();
-      }
-  });
-  $('.scrollUpButton').click(function(){
-      $("html, body").animate({ scrollTop: 0 }, 500);
-      return false;
-  });
- });
